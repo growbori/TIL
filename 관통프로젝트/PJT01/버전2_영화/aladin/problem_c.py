35e77a635b4b4923e065300e1183833384160e16
gitnamgyeonglee <lurelight@naver.com>
2024-01-20 19:43:50 +0900

    pjt;

--- 관통프로젝트/PJT01/버전2_영화/aladin/problem_c.py
+++ 관통프로젝트/PJT01/버전2_영화/aladin/problem_c.py
@@ -4,16 +4,17 @@ from pprint import pprint
 
 def books_info(books, categories):
     cata_Name = []
-    book_data = []
+    books_data = []
     for i in range (0, len(categories)):
         for j in range(len(books[0]['categoryId'])):
 
             if books[0]['categoryId'][j] == categories[i]['id']:
                 cata_Name.append(categories[i]['name']) # 문학, 영미소설 둘다 들어오게 하는 방법 고민해보기!
                 
-                book_data.append({'author' : books[0]['author'], 'categoryName' : cata_Name, 'cover' : books[0]['cover'], 'description' : books[0]['description'], 'id' : books[0]['id'], 'priceSales' : books[0]['priceSales'], 'title' : books[0]['title']})
+                for k in range(len(books[0])):
+                    books_data.append({'author' : books[k]['author'], 'categoryName' : cata_Name, 'cover' : books[k]['cover'], 'description' : books[k]['description'], 'id' : books[k]['id'], 'priceSales' : books[k]['priceSales'], 'title' : books[k]['title']})
 
-    return book_data
+    return books_data
 
 
 # 아래의 코드는 수정하지 않습니다.
